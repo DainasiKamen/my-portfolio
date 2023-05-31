9cd08d74f21eb8ca8d836c53035b740e48db2ede
ユãhiguchishota <dainasikamen@gmail.com>
2023-05-31 15:57:38 +0000

    fix:背景の追加
    (cherry picked from commit 0f9490ecf2a12bff9784bdbd9496a349fb2fec33)

--- app/my-portfolio/src/App.vue
+++ app/my-portfolio/src/App.vue
@@ -4,14 +4,20 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <video id="video" autoplay="true" muted="true" loop="true"
+      src="./assets/Mouthpiece.mp4">
+    </video>
+
+    <div class="mv-txt">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+      <HelloWorld msg="Vite + Vue"/>
+    </div>
+  </div> 
 </template>
 
 <style scoped>
@@ -26,5 +32,29 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+video {
+  display: block;
+  width: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+}
+
+.mv-txt {
+  color: #fff;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  /* レイヤー背景の上に乗せる */
+  z-index: 1;
+  margin: auto;
+  text-align: center;
 }
 </style>
